56b275ad0ac914121d1715dfab903c5d84dc3a3c
Marcin Andrzejczak <amostori@op.pl>
2023-06-11 17:54:20 +0200

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_fire_tutorial/pages/StoreListPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
@@ -14,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text(''),
+      home: const StoreListPage(),
     );
   }
 }
---
+++ lib/pages/StoreListPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StoreListPage extends StatelessWidget {
+  const StoreListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
