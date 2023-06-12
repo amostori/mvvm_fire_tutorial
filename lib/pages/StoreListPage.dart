87d1352b75177686ed40139ea35bef001db35949
Marcin Andrzejczak <amostori@op.pl>
2023-06-12 10:11:29 +0200

    _buildBody() done

--- lib/pages/StoreListPage.dart
+++ lib/pages/StoreListPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mvvm_fire_tutorial/view_models/store_list_vm.dart';
 
@@ -27,5 +28,20 @@ class StoreListPage extends StatelessWidget {
 
   void _navigateToAddStorePage(BuildContext context) {}
 
-  _buildBody() {}
+  Widget _buildBody() => StreamBuilder<QuerySnapshot>(
+      stream: _storeListViewModel.storesAsStream,
+      builder: (context, snapshot) {
+        // snapshot.data to pojedyńczy QuerySnapshot z Firebase.
+        // QuerySnapshot zawiera wynik zapytania
+        QuerySnapshot? querySnapshot = snapshot.data;
+        if (snapshot.hasData && querySnapshot!.docs.isNotEmpty) {
+          return _buildList(querySnapshot);
+        } else {
+          return const Text('NO_STORES_FOUND');
+        }
+      });
+
+  Widget _buildList(QuerySnapshot querySnapshot) {
+    return const Text('ListView.builder');
+  }
 }
