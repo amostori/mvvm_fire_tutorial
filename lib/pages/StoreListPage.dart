cae898d27a2fd637453be41a2f6b4a52f9a9c4c2
Marcin Andrzejczak <amostori@op.pl>
2023-06-12 10:06:50 +0200

    StoreListPage barebones

--- lib/pages/StoreListPage.dart
+++ lib/pages/StoreListPage.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_fire_tutorial/view_models/store_list_vm.dart';
 
 class StoreListPage extends StatelessWidget {
-  const StoreListPage({Key? key}) : super(key: key);
+  StoreListPage({Key? key}) : super(key: key);
+  final StoreListViewModel _storeListViewModel = StoreListViewModel();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Grocery App"),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                child: const Icon(Icons.add),
+                onTap: () {
+                  _navigateToAddStorePage(context);
+                },
+              ),
+            )
+          ],
+        ),
+        body: _buildBody());
   }
-}
 
+  void _navigateToAddStorePage(BuildContext context) {}
+
+  _buildBody() {}
+}
